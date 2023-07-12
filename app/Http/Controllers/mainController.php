42ae6d76a24798ab396b871e56a874aea1fbcfe8
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-07-12 17:03:08 +0200

    cards and details on page

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -9,8 +9,7 @@ class mainController extends Controller
 {
     public function index() {
         $movies = Movie :: all();
-        dd($movies);
 
-        return view('welcome');
+        return view('welcome', compact('movies'));
     }
 }
